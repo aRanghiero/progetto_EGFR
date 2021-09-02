8186eddc4029ca4a0b7d3e8ec65979a314cb11ce
a.Ranghiero <alberto_ranghiero@hotmail.it>
2021-09-02 12:42:47 +0200

    script bash per filtrare i vcf al 5%

---
+++ script/scripBASH/VCF_AF5%.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#02/09/21
+
+#vedo dove mi trovo
+pwd
+
+# attivo l'environment di conda con il programma bcftools 
+# conda activate VCF2MAF 
+eval "$(conda shell.bash hook)"
+conda activate bcf_sam_vcf_tolls 
+
+mkdir filt5% # creo una cartella dove salvare i vcf filtrati
+
+# faccio eseguire un ciclo FOR per convertire tutti i VCF in MAF
+for file in *.vcf
+do
+	me=$(grep  -v "##" "$file" |  grep -v "chr" | awk -F ' ' '{print $10}') # prende l'id del paziente
+	echo $me
+	# filtro al 5%
+	bcftools view --min-af 0.05:nref "$file" --output-type v --output-file filt5%/"$(echo $file | sed 's/Non-Filtered/Filtered5_$me/g')" --threads 5 
+
+done
